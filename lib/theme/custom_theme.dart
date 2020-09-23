7cd7d8bec86a2d9ece0665cc3a3c4672dd5953e2
Olle Kyrychenko <oleksiikyrychenko@capdefier.com>
2020-09-23 15:33:09 +0300

    custom theme created

---
+++ lib/theme/custom_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/foundation.dart';
+import 'package:pictures_view/res/const.dart';
+import 'package:pictures_view/theme/data/default_theme.dart';
+import 'package:pictures_view/theme/models/appvesto_colors.dart';
+import 'package:pictures_view/theme/models/appvesto_text_styles.dart';
+
+import 'models/appvesto_theme.dart';
+
+class CustomTheme {
+  static const String tag = '[CustomTheme]';
+
+  CustomTheme._privateConstructor();
+
+  static final CustomTheme _instance = CustomTheme._privateConstructor();
+
+  static CustomTheme get instance => _instance;
+
+  AVTheme theme;
+
+  Never init(AVTheme initialTheme) {
+    theme = initialTheme ?? defaultTheme;
+  }
+
+  Never setNewTheme(AVTheme thm) {
+    logger.i('$tag => setNewTheme() => theme => ${thm.themeName}');
+    theme = thm;
+  }
+
+  Never setCustomTheme({@required String themeName, AVColors colors, AVTextStyles textStyles}) {
+    logger.i('$tag => setCustomTheme() => themeName => $themeName}');
+    theme.copyWith(
+      themeName: themeName,
+      colors: colors,
+      textStyles: textStyles,
+    );
+  }
+}
---
+++ lib/theme/models/appvesto_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/painting.dart';
+
+class AVColors {
+  final Color primaryColor;
+  final Color accentColor;
+
+  AVColors({
+    this.primaryColor,
+    this.accentColor,
+  });
+}
