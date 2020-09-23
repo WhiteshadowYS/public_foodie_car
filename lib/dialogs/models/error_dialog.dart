88736620105de81a9f5a1fb231236085e155271f
Yura <sirhikshevelev@gmail.com>
2020-09-23 11:57:53 +0300

    Added doc

--- lib/dialogs/models/error_dialog.dart
+++ lib/dialogs/models/error_dialog.dart
@@ -3,6 +3,13 @@ import 'package:pictures_view/dialogs/interfaces/i_dialog.dart';
 import 'package:pictures_view/dialogs/shared/dialog_builders.dart';
 import 'package:pictures_view/dialogs/widgets/error_dialog_widget.dart';
 
+/// [ErrorDialog] realisation for [IDialog] interface. This [dialog] was using for show all errors in the application.
+/// Params:
+///   - [title] - Title of dialog.
+///   - [message] - Main message(content) of dialog.
+/// Methods:
+///   - [_builder] - Private constructor for Dialog. Will use [DialogBuilders.defaultDialogBuilder] for show dialog.
+///   - [show] - @override function from [IDialog]. Will using for show dialog. Single [public] function.
 class ErrorDialog implements IDialog {
   final String title;
   final String message;
@@ -12,6 +19,7 @@ class ErrorDialog implements IDialog {
     this.title = 'Error',
   }) : assert(message != null, throw ('Message should be not null'));
 
+  /// [ErrorDialogWidget] it is widget for this dialog. What will accept a dialog object.
   Future<void> _builder(BuildContext context) {
     return DialogBuilders.defaultDialogBuilder(
       context: context,
