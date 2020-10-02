214e61908f9e7dd05825b1f2c529df783cf0fba3
Daniil <daniiladamyk@appvesto.com>
2020-10-02 16:21:47 +0300

    Fixed paddings on terms page

--- lib/ui/pages/terms_page/terms_page.dart
+++ lib/ui/pages/terms_page/terms_page.dart
@@ -3,37 +3,55 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:my_catalog/dictionary/dictionary_classes/terms_page_dictionary.dart';
 import 'package:my_catalog/dictionary/flutter_dictionary.dart';
 import 'package:my_catalog/store/application/app_state.dart';
+import 'package:my_catalog/theme/custom_theme.dart';
 import 'package:my_catalog/ui/layouts/main_layout/main_layout.dart';
 import 'package:my_catalog/ui/pages/terms_page/terms_page_vm.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TermsPage extends StatelessWidget {
   TermsPage() : super(key: Key('TermsPage'));
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, TermsPageVM>(
-      converter: TermsPageVM.fromStore,
-      builder: (BuildContext context, vm) {
-        final TermsPageDictionary dictionary = FlutterDictionary.instance.language.termsPageDictionary;
-        return MainLayout(
-          child: Padding(
-            padding: EdgeInsets.only(left: 16.w, right: 37.w, top: 50.h, bottom: 60.h,),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                // TODO(Daniil): Use theme textStyles
-                Text(dictionary.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize:18.0 ),),
-                Text(vm.termsText,),
-                Divider(thickness: 2.0,),
-                RaisedButton(
-                  child: Text(dictionary.agree),
-                  onPressed: () => vm.navigateToCatalogsPage(),
+        converter: TermsPageVM.fromStore,
+        builder: (BuildContext context, vm) {
+          final TermsPageDictionary dictionary = FlutterDictionary.instance.language.termsPageDictionary;
+          return MainLayout(
+            child: Container(
+              margin: EdgeInsets.only(
+                left: 16.w,
+                right: 37.w,
+                top: 50.h,
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+
+                    Text(
+                      dictionary.title,
+                      style: CustomTheme.textStyles.titleTextStyle(size: 18.0),
+                    ),
+                    SizedBox(height: 56.h),
+                    // TODO(Daniil): Use theme primary textStyle
+                    Text(
+                      vm.termsText,
+                    ),
+                    const SizedBox(height: 20),
+                    Divider(
+                      thickness: 1.5,
+                    ),
+                    RaisedButton(
+                      child: Text(dictionary.agree),
+                      onPressed: () => vm.navigateToCatalogsPage(),
+                    ),
+                    const SizedBox(height: 20),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        );
-      }
-    );
+          );
+        });
   }
 }
