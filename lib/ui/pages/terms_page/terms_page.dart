2e3f3abf37e9ee6576440bb2e78238fc3fba30a5
AndreyKaira <andreykaira@capdefier.com>
2020-10-02 16:42:06 +0300

    Fix

--- lib/ui/pages/terms_page/terms_page.dart
+++ lib/ui/pages/terms_page/terms_page.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:my_catalog/dictionary/dictionary_classes/terms_page_dictionary.dart';
+import 'package:my_catalog/dictionary/flutter_dictionary.dart';
 import 'package:my_catalog/store/application/app_state.dart';
+import 'package:my_catalog/theme/custom_theme.dart';
 import 'package:my_catalog/ui/layouts/main_layout/main_layout.dart';
 import 'package:my_catalog/ui/pages/terms_page/terms_page_vm.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'widgets/agree_button.dart';
 
