cd63436ea6593f76d3487af0b5b104ef872d8680
Daniil <daniiladamyk@appvesto.com>
2020-10-02 16:21:47 +0300

    Fixed paddings on terms page

--- lib/ui/pages/terms_page/terms_page.dart
+++ lib/ui/pages/terms_page/terms_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:my_catalog/dictionary/dictionary_classes/terms_page_dictionary.dart';
 import 'package:my_catalog/dictionary/flutter_dictionary.dart';
 import 'package:my_catalog/store/application/app_state.dart';
+import 'package:my_catalog/theme/custom_theme.dart';
 import 'package:my_catalog/ui/layouts/main_layout/main_layout.dart';
 import 'package:my_catalog/ui/pages/terms_page/terms_page_vm.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
