f12290b1ade68bc3491ef4b8b07864aba196c275
Daniil <daniiladamyk@appvesto.com>
2020-10-02 14:33:02 +0300

    Created dictionary for Terms page

--- lib/ui/pages/terms_page/terms_page.dart
+++ lib/ui/pages/terms_page/terms_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+import 'package:my_catalog/dictionary/dictionary_classes/terms_page_dictionary.dart';
+import 'package:my_catalog/dictionary/flutter_dictionary.dart';
 import 'package:my_catalog/store/application/app_state.dart';
+import 'package:my_catalog/theme/custom_theme.dart';
 import 'package:my_catalog/ui/layouts/main_layout/main_layout.dart';
 import 'package:my_catalog/ui/pages/terms_page/terms_page_vm.dart';
 
@@ -11,14 +14,19 @@ class TermsPage extends StatelessWidget {
     return StoreConnector<AppState, TermsPageVM>(
       converter: TermsPageVM.fromStore,
       builder: (BuildContext context, vm) {
+        final TermsPageDictionary dictionary = FlutterDictionary.instance.language.termsPageDictionary;
         return MainLayout(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Terms Page'),
-              const SizedBox(height: 24.0),
+              Text(dictionary.title, style: CustomTheme.textStyles.titleTextStyle(size: 18.0),),
+              Text('qweqweqweqweqweqwe\nqweqweqweqweqweqwe\nqweqweqweqweqweqwe\nqweqweqweqweqweqwe\nqweqweqweqweqweqwe \n qweqweqweqweqweqwe \nqweqweqweqweqweqwe \n qweqweqweqweqweqwe \n'),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Divider(thickness: 2.0,),
+              ),
               RaisedButton(
-                child: Text('To Catalogs Page'),
+                child: Text(dictionary.agree),
                 onPressed: () => vm.navigateToCatalogsPage(),
               ),
             ],
