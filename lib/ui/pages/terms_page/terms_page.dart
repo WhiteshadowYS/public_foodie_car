b9da05560e59f84f21e08f79e69f38a8196d98dc
Daniil <daniiladamyk@appvesto.com>
2020-10-02 14:55:56 +0300

    Created terms page

--- lib/ui/pages/terms_page/terms_page.dart
+++ lib/ui/pages/terms_page/terms_page.dart
@@ -3,9 +3,9 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:my_catalog/dictionary/dictionary_classes/terms_page_dictionary.dart';
 import 'package:my_catalog/dictionary/flutter_dictionary.dart';
 import 'package:my_catalog/store/application/app_state.dart';
-import 'package:my_catalog/theme/custom_theme.dart';
 import 'package:my_catalog/ui/layouts/main_layout/main_layout.dart';
 import 'package:my_catalog/ui/pages/terms_page/terms_page_vm.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class TermsPage extends StatelessWidget {
   TermsPage() : super(key: Key('TermsPage'));
@@ -16,20 +16,21 @@ class TermsPage extends StatelessWidget {
       builder: (BuildContext context, vm) {
         final TermsPageDictionary dictionary = FlutterDictionary.instance.language.termsPageDictionary;
         return MainLayout(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(dictionary.title, style: CustomTheme.textStyles.titleTextStyle(size: 18.0),),
-              Text('qweqweqweqweqweqwe\nqweqweqweqweqweqwe\nqweqweqweqweqweqwe\nqweqweqweqweqweqwe\nqweqweqweqweqweqwe \n qweqweqweqweqweqwe \nqweqweqweqweqweqwe \n qweqweqweqweqweqwe \n'),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Divider(thickness: 2.0,),
-              ),
-              RaisedButton(
-                child: Text(dictionary.agree),
-                onPressed: () => vm.navigateToCatalogsPage(),
-              ),
-            ],
+          child: Padding(
+            padding: EdgeInsets.only(left: 16.w, right: 37.w, top: 50.h, bottom: 60.h,),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                // TODO(Daniil): Use theme textStyles
+                Text(dictionary.title, style: TextStyle(fontWeight: FontWeight.bold, fontSize:18.0 ),),
+                Text(vm.termsText,),
+                Divider(thickness: 2.0,),
+                RaisedButton(
+                  child: Text(dictionary.agree),
+                  onPressed: () => vm.navigateToCatalogsPage(),
+                ),
+              ],
+            ),
           ),
         );
       }
