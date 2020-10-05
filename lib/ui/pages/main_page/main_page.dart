e17486fdfc5dc1c3b91d7eff39bd17369a23a857
Yura <sirhikshevelev@gmail.com>
2020-10-05 10:26:08 +0300

    Added showDialog button.

--- lib/ui/pages/main_page/main_page.dart
+++ lib/ui/pages/main_page/main_page.dart
@@ -9,6 +9,7 @@ import 'package:my_catalog/ui/shared/app_bar/main_app_bar.dart';
 
 class MainPage extends StatelessWidget {
   MainPage() : super(key: Key('MainPage'));
+
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, MainPageVM>(
@@ -28,21 +29,28 @@ class MainPage extends StatelessWidget {
               const SizedBox(height: 24.0),
               RaisedButton(
                 child: Text('To Terms Page'),
-                onPressed: () => DialogService.instance.show(NotificationDialog(
-                  title: 'Eternal',
-                  message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-                )),
-//                onPressed: () => vm.navigateToTermsPage(),
+                onPressed: () => vm.navigateToTermsPage(),
               ),
               const SizedBox(height: 24.0),
               RaisedButton(
                 child: Text('Get Storage Data'),
                 onPressed: () => vm.getData(),
               ),
+              const SizedBox(height: 24.0),
+              RaisedButton(
+                child: Text('Get Storage Data'),
+                onPressed: () => DialogService.instance.show(
+                  NotificationDialog(
+                    title: 'Eternal',
+                    message:
+                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+                  ),
+                ),
+              ),
             ],
           ),
         );
-      }
+      },
     );
   }
 }
