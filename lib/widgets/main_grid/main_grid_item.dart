3292fe8eb1cbd61f0cbb7dbb295473b12729e6ef
AndreyKaira <andreykaira@capdefier.com>
2020-10-13 10:58:15 +0300

    Update categories

---
+++ lib/widgets/main_grid/main_grid_item.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:my_catalog/res/image_assets.dart';
+import 'package:my_catalog/theme/custom_theme.dart';
+
+class CategoryItem extends StatefulWidget {
+  final int index;
+  final String title;
+  final String imageUrl;
+  final void Function() onTap;
+
+  CategoryItem({
+    @required String key,
+    @required this.imageUrl,
+    @required this.title,
+    @required this.index,
+    @required this.onTap,
+  }) : super(key: Key(key));
+
+  @override
+  _CategoryItemState createState() => _CategoryItemState();
+}
+
+class _CategoryItemState extends State<CategoryItem> with SingleTickerProviderStateMixin {
+  bool _init = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _init = true;
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedOpacity(
+      duration: Duration(milliseconds: 500 + (100 * widget.index % 10)),
+      opacity: _init ? 1.0 : 0.0,
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(18.0),
+          splashColor: CustomTheme.colors.primaryColor.withOpacity(0.3),
+          highlightColor: CustomTheme.colors.primaryColor.withOpacity(0.2),
+          onTap: widget.onTap,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: FadeInImage(
+                  height: 190.0,
+                  placeholder: AssetImage(ImageAssets.LOGO_PNG),
+                  image: NetworkImage(widget.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(height: 8.0),
+              Text(
+                widget.title,
+                //widget.category.languages[FlutterDictionaryDelegate.getCurrentLocale.toUpperCase()][NAME],
+                style: CustomTheme.textStyles.titleTextStyle(size: 15.0),
+                textAlign: TextAlign.left,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
