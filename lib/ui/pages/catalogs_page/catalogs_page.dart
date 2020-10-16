6f7a65c15b5f701c55dd7bdd503ee19e2ab04bc6
Olle Kyrychenko <oleksiikyrychenko@capdefier.com>
2020-10-16 07:58:07 +0300

    merged

--- lib/ui/pages/catalogs_page/catalogs_page.dart
+++ lib/ui/pages/catalogs_page/catalogs_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_catalog/models/models/storage_model/data/data/catalog_model.dart';
 import 'package:my_catalog/res/keys.dart';
 import 'package:my_catalog/store/application/app_state.dart';
 import 'package:my_catalog/theme/custom_theme.dart';
@@ -8,7 +9,6 @@ import 'package:my_catalog/ui/layouts/main_layout/main_layout.dart';
 import 'package:my_catalog/ui/pages/catalogs_page/catalogs_page_vm.dart';
 import 'package:my_catalog/ui/pages/catalogs_page/widgets/catalog_item.dart';
 import 'package:my_catalog/ui/shared/app_bar/main_app_bar.dart';
-import 'package:my_catalog/ui/shared/svg_images.dart';
 import 'package:my_catalog/utils/clean_behavior.dart';
 
 class CatalogsPage extends StatelessWidget {
