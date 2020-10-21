2d7a27603ba6b3d345185cb33a112209f616515f
AndreyKaira <andreykaira@capdefier.com>
2020-10-21 15:51:54 +0300

    Fix main page

--- lib/dictionary/dictionary_classes/error_dictionary.dart
+++ lib/dictionary/dictionary_classes/error_dictionary.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 
 class ErrorDictionary {
   final String isNumberError;
+  final String isNegativeNumIdError;
+  final String isZeroNumIdError;
 
   const ErrorDictionary({
     @required this.isNumberError,
+    @required this.isNegativeNumIdError,
+    @required this.isZeroNumIdError,
   });
 }
