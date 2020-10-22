dc439d77638f35fae331e82f24e413a08330700a
Olle Kyrychenko <oleksiikyrychenko@capdefier.com>
2020-10-22 16:23:50 +0300

    added get data loader

--- lib/services/dialog_service/widgets/loader_dialog_widget.dart
+++ lib/services/dialog_service/widgets/loader_dialog_widget.dart
@@ -5,6 +5,7 @@ import 'package:my_catalog/res/app_styles/app_colors.dart';
 import 'package:my_catalog/res/image_assets.dart';
 import 'package:my_catalog/services/dialog_service/models/default_loader_dialog.dart';
 import 'package:my_catalog/theme/custom_theme.dart';
+import 'package:my_catalog/widgets/fade_animation_container.dart';
 
 class LoaderDialogWidget extends StatelessWidget {
   final DefaultLoaderDialog loaderModel;
@@ -17,29 +18,7 @@ class LoaderDialogWidget extends StatelessWidget {
       width: double.infinity,
       height: double.infinity,
       color: AppColors.kBlack.withOpacity(0.3),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SvgPicture.asset(
-            ImageAssets.LOADING,
-            height: 50.0,
-            width: 50.0,
-            color: CustomTheme.colors.primaryColor,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(
-              top: 16.0,
-            ),
-            child: Material(
-              color: Colors.transparent,
-              child: Text(
-                FlutterDictionary.instance.language.popUpDictionary.loading,
-                style: TextStyle(color: AppColors.kWhite),
-              ),
-            ),
-          ),
-        ],
-      ),
+      child: FadeAnimationContainer(),
     );
   }
 }
